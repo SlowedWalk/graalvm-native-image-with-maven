07a738f38ce7f6d91f1215f555b0ed2d08320392
Hidetora <idetoratojo1@gmail.com>
2024-02-06 14:21:09 +0100

    init

--- src/main/java/tech/hidetora/graalvmnativeimage/GraalvmNativeImageApplication.java
+++ src/main/java/tech/hidetora/graalvmnativeimage/GraalvmNativeImageApplication.java
@@ -1,13 +1,10 @@
 package tech.hidetora.graalvmnativeimage;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import tech.hidetora.graalvmnativeimage.entity.Customer;
-import tech.hidetora.graalvmnativeimage.repository.CustomerRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,20 +15,30 @@ public class GraalvmNativeImageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GraalvmNativeImageApplication.class, args);
 	}
-	@Bean
-	@Profile("!test")
-	CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
-		log.info("================= Initialization ================");
-		return args -> {
-			List<Customer> customers = List.of(
-					Customer.builder()
-							.firstName("Hidetora").lastName("Tojo").email("hidetoratojo@gmail.com").build(),
-					Customer.builder()
-							.firstName("Aomine").lastName("Diaki").email("diaki.aomine@gmail.com").build(),
-					Customer.builder()
-							.firstName("Hanane").lastName("yamal").email("hanane@gmail.com").build()
-			);
-			customerRepository.saveAll(customers);
-		};
+
+}
+
+@RestController
+class Controller {
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return List.of(
+				new Customer(1L, "John", "Doe", "johndoe@gmail.com"),
+				new Customer(2L, "Jane", "Doe", "janedoe@gmail.com"),
+				new Customer(3L, "Jack", "Doe", "jack@gmail.com")
+		);
+	}
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to GraalVM Native Image";
 	}
 }
+
+record Customer(
+		Long id,
+		String firstName,
+		String lastName,
+		String email
+) { }
+
